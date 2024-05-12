eff434bc76699c85e9de3736dcc884581db3dd61
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2024-05-12 10:49:06 +0530

    Updated the Commands.sh

--- command.sh
+++ command.sh
@@ -1,5 +1,27 @@
 # This file contains the cmd commands that I used while working... this is more like notes for cmd and aws cli
 
-# cd "C:\Program Files\MySQL\MySQL Shell 8.0\bin\"
+# Commands to run on Amazon AMI EC2 Instance:
 
-mysql -h demo-task-db.cd2ya642ewvu.ap-south-1.rds.amazonaws.com -P 3306 -u mymasteruser -p    
+# Install node version manager (nvm) by typing the following at the command line.
+sudo su -
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+# Activate nvm by typing the following at the command line.
+. ~/.nvm/nvm.sh
+
+# Use nvm to install the latest version of Node.js by typing the following at the command line.
+nvm install node
+
+# Test that node and npm are installed and running correctly by typing the following at the terminal:
+node -v
+npm -v
+
+# To install git, run below commands in the terminal window:
+yum update -y
+yum install git -y
+
+# Just to verify if system has git installed or not, please run below command in terminal:
+git --version
+
+# Clone the repo to be deployed:
+git clone https://github.com/AmanSinghBhogal/AWS-RDS-Demo.git
